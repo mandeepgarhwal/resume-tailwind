52ff34c89e99c200308df980f9d22fc4c7f5b11e
mandeepgarhwal <114212567+mandeepgarhwal@users.noreply.github.com>
2024-02-06 16:01:11 +0530

    Add files via upload

--- src/projects.jsx
+++ src/projects.jsx
@@ -19,22 +19,21 @@ import cine from "./cinema.jpg"
 
 export default function Projects() {
   return (
-    <div className='bg-red-200 pt-5 pl-10'>
-      <MDBContainer fluid>
+    <div className='bg-red-200 pt-5 sm:pl-10'>
+      <MDBContainer fluid  className=' -ml-4  sm:mx-0'>
         <MDBRow center className='my-5'>
           <MDBCol size={5}>
             <div>
-              <MDBCard style={{ maxWidth: '540px' }}>
+              <MDBCard style={{ maxWidth: '540px', minWidth: "170px" }} className="min-h-[442px] sm:min-h-[210px]">
                 <MDBRow className='g-0'>
                   <MDBCol md='4'>
                     <MDBCardImage src={foodzap} alt='...' fluid className='w-full h-full' />
                   </MDBCol>
                   <MDBCol md='8'>
                     <MDBCardBody>
-                      <MDBCardTitle className='text-xl font-bold'>Food Zap</MDBCardTitle>
-                      <MDBCardText>
+                      <MDBCardTitle className='text-xs sm:text-xl font-bold'>Food Zap</MDBCardTitle>
+                      <MDBCardText className='text-xs sm:text-base'>
                         A food ordering app with focus on user experience and adaptability to user tastes. Next slide will contain Few principles to be followed in app design which demonstrates focus on user experience
-
                       </MDBCardText>
                       <MDBCardText>
                         <small className='text-muted'>2022</small>
@@ -46,16 +45,16 @@ export default function Projects() {
             </div>
           </MDBCol>
           <MDBCol size={5}>
-            <div>
-              <MDBCard style={{ maxWidth: '540px' }}>
+            <div >
+              <MDBCard style={{ maxWidth: '540px', minWidth: "170px" }} className="min-h-[442px] sm:min-h-[210px]" >
                 <MDBRow className='g-0'>
                   <MDBCol md='4'>
                     <MDBCardImage src={explearn} alt='...' fluid className='w-full h-full' />
                   </MDBCol>
                   <MDBCol md='8'>
                     <MDBCardBody>
-                      <MDBCardTitle className='text-xl font-bold'>Experiencial Learning</MDBCardTitle>
-                      <MDBCardText>
+                      <MDBCardTitle className='text-xs sm:text-xl font-bold'>Experiencial Learning</MDBCardTitle>
+                      <MDBCardText className='text-xs sm:text-base'>
                         I am proposing rearrangement of current
                         popular learning environment to correct the prevailing order of
                         type of learning to take it closer to optimum
@@ -71,20 +70,19 @@ export default function Projects() {
               </MDBCard>
             </div>
           </MDBCol>
-
         </MDBRow>
         <MDBRow around className='my-5'>
           <MDBCol size={5}>
             <div>
-              <MDBCard style={{ maxWidth: '540px' }}>
+              <MDBCard style={{ maxWidth: '540px', minWidth: "170px" }} className="min-h-[442px] sm:min-h-[210px]">
                 <MDBRow className='g-0'>
                   <MDBCol md='4'>
                     <MDBCardImage src= {lms} alt='...' fluid  className='w-full h-full'/>
                   </MDBCol>
                   <MDBCol md='8'>
                     <MDBCardBody>
-                      <MDBCardTitle className='text-xl font-bold'> Learning Management system</MDBCardTitle>
-                      <MDBCardText>
+                      <MDBCardTitle className='text-xs sm:text-xl font-bold'> Learning Management system</MDBCardTitle>
+                      <MDBCardText className='text-xs sm:text-base'>
                         It is proposed to be a online AI enabled learning managemant system which will supplement kearning oucomes of the students by providing use knowledge and practice to him and also geenrate helpful indicators for his teachers
                       </MDBCardText>
                       <MDBCardText>
@@ -98,15 +96,15 @@ export default function Projects() {
           </MDBCol>
           <MDBCol size={5}>
             <div>
-              <MDBCard style={{ maxWidth: '540px' }}>
+              <MDBCard style={{ maxWidth: '540px', minWidth: "170px" }} className="min-h-[442px] sm:min-h-[210px]">
                 <MDBRow className='g-0'>
                   <MDBCol md='4'>
                     <MDBCardImage src= {dazzle} alt='...' fluid className='w-full h-full'/>
                   </MDBCol>
                   <MDBCol md='8'>
                     <MDBCardBody>
-                      <MDBCardTitle className='text-xl font-bold'>Dazzle classes</MDBCardTitle>
-                      <MDBCardText>
+                      <MDBCardTitle className='text-xs sm:text-xl font-bold'>Dazzle classes</MDBCardTitle>
+                      <MDBCardText className='text-xs sm:text-base'>
                         Dazzle Classes is spearheaded by a young dynamic team of professionals from the Medical & Engineering background and has wide experience in the field of Education & Career Counseling. Dazzle Classes is setup with a vision.
                       </MDBCardText>
                       <MDBCardText>
@@ -122,15 +120,15 @@ export default function Projects() {
         <MDBRow between className='my-5'>
           <MDBCol size={5}>
             <div>
-              <MDBCard style={{ maxWidth: '540px' }}>
+              <MDBCard style={{ maxWidth: '540px', minWidth: "170px" }} className="min-h-[442px] sm:min-h-[210px]">
                 <MDBRow className='g-0'>
                   <MDBCol md='4'>
                     <MDBCardImage src= {mmi} alt='...' fluid className='w-full h-full'/>
                   </MDBCol>
                   <MDBCol md='8'>
                     <MDBCardBody>
-                      <MDBCardTitle className='text-xl font-bold'>Man and Machine Interface</MDBCardTitle>
-                      <MDBCardText>
+                      <MDBCardTitle className='text-xs sm:text-xl font-bold'>Man and Machine Interface</MDBCardTitle>
+                      <MDBCardText className='text-xs sm:text-base'>
                         This projects aims to bridge the gap between man and machines he use by making their communication simple and effortless by mapping brain activity using invasive and non invasive techinques and give him ability to control machines through it.
                       </MDBCardText>
                       <MDBCardText>
@@ -144,15 +142,15 @@ export default function Projects() {
           </MDBCol>
           <MDBCol size={5}>
             <div>
-              <MDBCard style={{ maxWidth: '540px' }}>
+              <MDBCard style={{ maxWidth: '540px', minWidth: "170px" }} className="min-h-[442px] sm:min-h-[210px]">
                 <MDBRow className='g-0'>
                   <MDBCol md='4'>
                     <MDBCardImage src= {phd} alt='...' fluid className='w-full h-full'/>
                   </MDBCol>
                   <MDBCol md='8'>
                     <MDBCardBody>
-                      <MDBCardTitle className='text-xl font-bold -mt-1'>India and china: Historical Reverberations</MDBCardTitle>
-                      <MDBCardText >
+                      <MDBCardTitle className='text-xs sm:text-xl font-bold -mt-1'>India and china: Historical Reverberations</MDBCardTitle>
+                      <MDBCardText className='text-xs sm:text-base'>
                         This is as thesis on India and China relation by taking both not as countries only but rather as civilisations to better understand 20 centuries on interactions far earlier then Treaty of Westphalia. 
                       </MDBCardText>
                       <MDBCardText>
@@ -168,15 +166,15 @@ export default function Projects() {
         <MDBRow center className='mt-5'>
           <MDBCol size={5}>
             <div>
-              <MDBCard style={{ maxWidth: '540px' }}>
+              <MDBCard style={{ maxWidth: '540px', minWidth: "170px" }}>
                 <MDBRow className='g-0'>
                   <MDBCol md='4'>
                     <MDBCardImage src= {cine} alt='...' fluid className='h-full w-full'/>
                   </MDBCol>
                   <MDBCol md='8'>
                     <MDBCardBody>
-                      <MDBCardTitle className='text-xl font-bold'>Cinema and India: Fate intertwined</MDBCardTitle>
-                      <MDBCardText>
+                      <MDBCardTitle className='text-xs sm:text-xl font-bold'>Cinema and India: Fate intertwined</MDBCardTitle>
+                      <MDBCardText className='text-xs sm:text-base'>
                         This project is in form of book exploring evolution of indian cinema since independence in context with changes in indian society and explore how they affected each other sometimes for better and sometime for worse.
                       </MDBCardText>
                       <MDBCardText>
